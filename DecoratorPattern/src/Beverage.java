2b7c3898bf822bd4c76445322d0247c5e25ec6ff
nora-wael <85520772+nora-wael@users.noreply.github.com>
2024-04-27 20:06:04 +0300

    Add files via upload

---
+++ DecoratorPattern/src/Beverage.java
@@ -0,0 +1,7 @@
+public abstract class Beverage {
+    String description="unknown beverage ";
+    public String getDescription(){
+        return description;
+    }
+    public abstract double cost();
+}
---
+++ DecoratorPattern/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+         Beverage beverage= new Espresso();
+        System.out.println(beverage.getDescription()+"$"+beverage.cost());
+
+    }
+}
